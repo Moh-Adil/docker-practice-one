72dcb632ff126f0974f6d20f8a138493fb4381ad
moh-adil <adilrabbani99@gmail.com>
2026-09-16 19:25:41 +0100

    A: adding better-sqlite3 and dummy data

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import db from './db.js';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/users', (req, res) => {
+  const stmt = db.prepare("SELECT * FROM users");
+  const users = stmt.all();
+  res.json(users);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
